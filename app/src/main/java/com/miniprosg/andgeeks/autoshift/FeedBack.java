c7c6b3cddfe6515c2da40632657369dd3c03d13d
AnujithVinod <anujithvinod99@gmail.com>
2018-11-17 22:41:21 +0530

    FINAL COMMIT............
    PROJECT FINISHED> ALL MODULES ARE WORKING>
    
    TEAM MEMBERS:
    1. ANUJITH N VINOD
    2. DEVIKA ADHITHYU
    3. ARYA REJI
    4. ANJU SUSAN THOMAS
    
    17/11/2018

--- app/src/main/java/com/miniprosg/andgeeks/autoshift/FeedBack.java
+++ app/src/main/java/com/miniprosg/andgeeks/autoshift/FeedBack.java
@@ -4,7 +4,9 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -33,6 +35,16 @@ public class FeedBack extends AppCompatActivity {
         Name = (EditText) findViewById(R.id.fname);
         Message = (EditText) findViewById(R.id.fcomplaint);
         setTitle("Feedbacks and Suggesions");
+        findViewById(R.id.feedback).setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return false;
+            }
+        });
 
     }
 
